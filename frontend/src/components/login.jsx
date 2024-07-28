5473358ea0a88b7fb5288bc76d1e321dba05febd
CHEGEBB <chegephil24@gmail.com>
2024-07-28 21:00:29 +0300

    new

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,6 +1,12 @@
 const LoginPage = () => {
     return ( 
-        <div className="login"></div>
+        <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+            <p>Don't have an account? <a href="#">Sign Up</a></p>
+        </div>
      );
 }
  
