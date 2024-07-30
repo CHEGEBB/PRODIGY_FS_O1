23666a70ef866eeb3e9395152ecda05b13de648d
CHEGEBB <chegephil24@gmail.com>
2024-07-30 19:36:16 +0300

    refactor: Update index.js to import index.scss for styling

---
+++ frontend/src/components/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import "../sass/Auth.scss"
+const AuthPage = () => {
+    return ( 
+        <div className="container"></div>
+        
+     );
+}
+ 
+export default AuthPage;
