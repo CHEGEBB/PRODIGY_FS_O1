e20a2f74a4c96e2f56e1058e7082adf539ee07d2
CHEGEBB <chegephil24@gmail.com>
2024-07-31 15:15:50 +0300

    refactor: Add email and password fields to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,4 +5,15 @@ const  userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    email :{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password :{
+        type: String,
+        required: true
+    }
 });
+
+const User = mongoose.model('User', userSchema);
